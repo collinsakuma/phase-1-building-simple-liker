7a189bb054d6d4aab15c20d54b51ee846bcc4a2a
Collin Sakuma <collinmsakuma@gmail.com>
2023-02-04 14:18:14 -0800

    lab complete

--- main.js
+++ main.js
@@ -3,9 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heart = document.querySelectorAll('span.like-glyph')
+const errorMessage = document.querySelector('#modal')
+console.log(heart)
+
+heart.forEach((like => like.addEventListener('click', likeCallback)))
 
 
+function likeCallback(like) {
+  //console.log(like.target)
+  mimicServerCall()
+    .then(() => {
+      if (like.target.innerText === EMPTY_HEART) {
+        like.target.innerText = FULL_HEART
+      } else if (like.target.innerText === FULL_HEART) {
+        like.target.innerText = EMPTY_HEART
+      }
+    })
+    .catch(() => {
+      const errorMessage = document.querySelector('#modal')
+      errorMessage.className = 'show'
 
+      setTimeout(() => {
+        errorMessage.className = 'hidden'
+      }, 3000)
+    })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
